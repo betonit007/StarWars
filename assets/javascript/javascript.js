1386871aaa7301ea08d7d4e14086bf69139d99df
Susan Nagorski <timnagorski@hotmail.com>
2018-09-24 17:28:27 -0400

    second round

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -6,28 +6,29 @@ function aa() {
 
 /////////////Players on Click/////////////////
 $(document).ready(function() {
-    $(".player").on("click", function(event) {
+    $(".player").on("click", function(event) {   
     ///////get attacker////////
     var attacker = event.target;
-    console.log(attacker.id);
-    //////get defenders////////
-    var pool = $(".player");
-    console.log(pool);
-    console.log(pool[1].id);
-    console.log(pool.length);
-    var defenders = [];
-    for (var i = 0; i < pool.length; i++) {
-        if (attacker.id !== pool[i].id) {
-            var next = document.getElementById(pool[i].id);
-            console.log(next);
-            $(next).removeClass("player");
-            $(next).addClass("villian");
-            $("#enemyfield").append(next);
+    console.log(attacker.className);
+    if (attacker.className === "player box") {
+       $(attacker).removeClass("player");
+       //////get defenders////////
+       var pool = $(".player");
+       var defenders = [];
+       for (var i = 0; i < pool.length; i++) {
+           if (attacker.id !== pool[i].id) {
+               var next = document.getElementById(pool[i].id);
+               $(next).addClass("villian");
+               $("#enemyfield").append(next);
 
-           
-        }
+           }
+       }
+    }
+    
+    if (attacker.className === "player box villian") {
+      console.log(attacker);
     }
 
     });
-    
-});
+
+});  
